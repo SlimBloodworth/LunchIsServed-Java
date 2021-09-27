63a2ed8f90b51cfb9db7f6dc77eaed842a59e986
Michelle Bloodworth <83024708+SlimBloodworth@users.noreply.github.com>
2021-09-27 12:06:54 -0500

    Add files via upload

---
+++ LunchIsServed/LunchIsServed.java
@@ -0,0 +1,15 @@
+public class LunchIsServed
+{
+	public static void main (String []args)
+	{
+		System.out.println("   ________");
+		System.out.println("      /");
+		System.out.println("  0 /");
+		System.out.println(" /|          __________");
+		System.out.println("| |         /         /|");
+		System.out.println(" / \\      /_________/  |");
+		System.out.println("/   \\    |         |   |");
+		System.out.println("         |         |");
+		System.out.println(" -Lunch is served!");
+		}
+	}
